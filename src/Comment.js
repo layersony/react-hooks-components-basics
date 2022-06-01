a566158e716c370e750750e7485c86e12fe4ac47
layersony <layersony@gmail.com>
2022-06-01 04:17:46 +0300

    Complete Assignment

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
+  return (
+      <div>Naturally, I agree with this article.</div>
+  );
 }
 
 export default Comment;
